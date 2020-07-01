93763d938e0d74575724e5e58f649a5f343da5e2
chaoabc <1312998662@qq.com>
2020-07-01 22:43:21 +0800

    第二次提交：其他合并单元格均取首个单元格的值

--- samples/read_excel.py
+++ samples/read_excel.py
@@ -15,16 +15,20 @@ wb =xlrd.open_workbook(excel_path)
 sheet =wb.sheet_by_name('Sheet1')
 cell_value =sheet.cell_value(1,0)  # 合并的单元格，值在单元格的0,0  1,0 2,0 3,0……值为空
 print(cell_value)
-print(sheet.merged_cells)    #返回一个列表   起始行、结束行、起始列、结束列
 
+#思路：凡是在merged_cells属性范围内的单元格，它的值均等于左上角首个单元格的值
+row_index = 2;col_index =0
+merged =sheet.merged_cells    #返回一个列表   起始行、结束行、起始列、结束列 [(1, 5, 0, 1)]
+print(sheet.merged_cells)
 def get_merged_cell_value(row_index,col_index):
     cell_value =None
-    for (srow,erow,scol,ecol) in merged:
-        if (srow<=row_index  and erow>row_index):
-            if (scol <=col_index  and ecol >col_index):
-                cell_value =sheet.cell_value(srow,erow)
+    for (srow,erow,scol,ecol) in merged:   #遍历表格中所有合并单元格的位置信息
+        if (srow<=row_index  and erow>row_index):      # 1<=2<5
+            if (scol <=col_index  and ecol >col_index):       # 0<=0<1
+                #如果满足条件，则把合并单元格首个单元格的值赋值给其他合并单元格
+                cell_value =sheet.cell_value(srow,scol)
     return cell_value
-print(get_merged_cell_value(2,0))
+print(get_merged_cell_value(4,0))
 
 
 
